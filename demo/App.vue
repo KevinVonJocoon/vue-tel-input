ff99c494eeef6402640cbfc181200cca473ef87b
Kevin <kevin@jocoon.de>
2019-01-18 11:07:39 +0100

    extended demo App.vue to make displayFormat and outputFormat dynamic (testing) and provide v-model value

--- demo/App.vue
+++ demo/App.vue
@@ -2,8 +2,21 @@
   <div id="app">
     <h1>Telephone Input Vue</h1>
     <h2 style="color: #999">made with &#x2764; by Steven.</h2>
+
+    <div>
+      Debug Config
+      <input type="text" v-model="displayFormat" placeholder="Display Format"/>
+      <input type="text" v-model="outputFormat" placeholder="Output Format"/>
+    </div>
+
     <div style="width: 500px; margin: 20px auto;">
-      <vue-tel-input @onInput="onInput" :preferredCountries="['us', 'gb', 'ua']"></vue-tel-input>
+      <vue-tel-input v-model="myPhoneNumber" @onInput="onInput" :preferredCountries="['us', 'gb', 'ua']" :displayFormat="displayFormat" :outputFormat="outputFormat"></vue-tel-input>
+    </div>
+    <div style="color: #e83e8c">
+      <span>
+        Output:
+        <strong>{{ myPhoneNumber }}</strong>,&nbsp;
+      </span>
     </div>
     <div v-if="phone.number" style="color: #e83e8c">
       <span>
@@ -27,11 +40,14 @@ export default {
   name: 'app',
   data() {
     return {
+      myPhoneNumber: '',
       phone: {
         number: '',
         isValid: false,
         country: undefined,
       },
+      displayFormat : 'International',
+      outputFormat : 'International'
     };
   },
   methods: {
